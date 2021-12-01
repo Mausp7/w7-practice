50643dd0038c0083fe167ef27d8c3e5d8336bf3f
Mausp7 <archie.tombacz@gmail.com>
2021-12-01 15:55:20 +0100

    Basic Sass added.

--- script.js
+++ script.js
@@ -16,42 +16,71 @@ functionName(1. 2);
 
  */
 
-const inputElement = (type, name, label) => {
+const inputElement = (type, name, label, placeholder="") => {
+    if (type === "checkbox") {
+        return `
+            <div>
+                <input type="${type}" name="${name}" id="${name}">
+                <label for="${name}"">${label}</label>
+            </div>
+        `;
+    } else {
+        return `
+            <div>
+                <label>${label}:</label>
+                <input type="${type}" name="${name}" placeholder="${placeholder}">
+            </div>
+        `;        
+    };    
+};
+
+const selectElement = (type, name, label, options) => {
+    let optionsToSelect = ``
+    for (const o of options) {
+        optionsToSelect += `<option>${o}</option>`
+    }
     return `
         <div>
             <label>${label}:</label>
-            <input type="${type}" name="${name}">
+            <${type} name="${name}">
+                ${optionsToSelect}
+            <${type}>
         </div>
     `;
 };
 
 const formElement = `
     <form id="form">
+        <h3>Jelentkezz ünnepi hírlevelünkre!</h3>
         ${inputElement("text", "firstName", "Keresztneved")}
+        ${inputElement("email", "personalEmail", "Emailcímed", "valami@host.hu")}
+        ${selectElement("select", "where", "Hol hallottál rólunk", ["Interneten", "Ismerőstől", "Egyéb"])}
+        ${inputElement("checkbox", "newsLetter", "Hírlevelet szeretnél kapni!")}
+        ${inputElement("checkbox", "terms", "Elfogadom a felhasználási feltételeket!")}
+        
         ${inputElement("file", "profilePicture", "Profilképed")}
-        ${inputElement("email", "personalEmail", "Emailcímed")}
-        ${inputElement("radio", "newsLetter", "Hírlevelet szeretnél kapni")}
-        ${inputElement("checkbox", "terms", "Elfogadom a felhasználási feltételeket")}
         <button>Ok</button>
     </form>
 `;
 
 const formSubmit = (event) => {
     event.preventDefault();
-    console.log(event.target);
-    event.target.classList.add("submitted");
+    const et = event.target;
+    console.log(et);
+    et.classList.add("submitted");
+
+    let selectValue = et.querySelector(`select[name="where"]`).value; 
+    console.log(selectValue);
 }
 
 const inputUpdate = (event) => {
     const inputValue = document.getElementById("inputValue");
     
-    if (inputValue.type === event.target.type) {
-        inputValue.innerHTML = event.target.value;
-        
-    } else if (inputValue.type === undefined) {
-        inputValue.type = event.target.type;
-        inputValue.innerHTML = event.target.value;
+    if (event.target.getAttribute("name") === "firstName") {
+        inputValue.innerHTML = `Üdv ${event.target.value}!`;
     };
+
+    console.log(event.target.closest(`#form`));
 }
 
 
@@ -61,11 +90,10 @@ function loadEvent() {
     const rootElement = document.getElementById("root");
     rootElement.insertAdjacentHTML("beforeend", formElement);
     
-    rootElement.insertAdjacentHTML("afterbegin", `
-        <div id="inputValue"></div>
+    rootElement.querySelector("h3").insertAdjacentHTML("beforebegin", `
+        <h2 id="inputValue">Üdv!</h2>
     `);
 
-    
     const form = document.getElementById("form");
     form.addEventListener("submit", formSubmit);
 
