ab0e00a5921bb4f26313622d5db6baef3d34104d
Mausp7 <archie.tombacz@gmail.com>
2021-12-02 15:56:07 +0100

    Input Element generator, Picture DZ preview added.

--- script.js
+++ script.js
@@ -16,19 +16,19 @@ functionName(1. 2);
 
  */
 
-const inputElement = (type, name, label, placeholder="") => {
+const inputElement = (type, name, label, placeholder="", required="") => {
     if (type === "checkbox") {
         return `
-            <div>
-                <input type="${type}" name="${name}" id="${name}">
+            <div class="${type}">
+                <input type="${type}" name="${name}" id="${name}" >
                 <label for="${name}"">${label}</label>
             </div>
         `;
     } else {
         return `
-            <div>
+            <div class="${type}">
                 <label>${label}:</label>
-                <input type="${type}" name="${name}" placeholder="${placeholder}">
+                <input type="${type}" name="${name}" placeholder="${placeholder}" ${required}>
             </div>
         `;        
     };    
@@ -49,19 +49,95 @@ const selectElement = (type, name, label, options) => {
     `;
 };
 
-const formElement = `
+const nameData = {
+    type: 'text',
+    name: 'firstName',
+    label: 'Keresztneved'
+}
+
+const anotherFormFields = [
+    {
+        type: 'text',
+        name: 'street',
+        label: 'Közterület neve'
+    },
+    {
+        type: 'number',
+        name: 'houseNumber',
+        label: 'Házszám',
+    },
+    {
+        type:"number",
+        name: "zipCode",
+        label: "Irányítószám"
+    },
+    {
+        type:"text",
+        name: "city",
+        label: "Település neve"
+    },
+]
+
+const formFields = [
+    {
+        type: 'text',
+        name: 'firstName',
+        label: 'Keresztneved'
+    },
+    {
+        type: 'email',
+        name: 'personalEmail',
+        label: 'Emailcímed',
+        placeholder: "valami@host.hu",
+        required: "required"
+    },
+    {
+        type:"checkbox",
+        name: "newsLetter",
+        label: "Hírlevelet szeretnél kapni!"
+    },
+    {
+        type:"checkbox",
+        name: "terms",
+        label: "Elfogadom a felhasználási feltételeket!"
+    },
+    {
+        type:"file",
+        name: "profilePicture",
+        label: "Profilképed"
+    }
+]
+
+/* const formElement = `
     <form id="form">
         <h3>Jelentkezz ünnepi hírlevelünkre!</h3>
-        ${inputElement("text", "firstName", "Keresztneved")}
-        ${inputElement("email", "personalEmail", "Emailcímed", "valami@host.hu")}
-        ${selectElement("select", "where", "Hol hallottál rólunk", ["Interneten", "Ismerőstől", "Egyéb"])}
+        ${inputElement(nameData.type, nameData.name, nameData.label)}
+        ${inputElement("email", "personalEmail", "Emailcímed", "valami@host.hu", "required")}
         ${inputElement("checkbox", "newsLetter", "Hírlevelet szeretnél kapni!")}
         ${inputElement("checkbox", "terms", "Elfogadom a felhasználási feltételeket!")}
-        
         ${inputElement("file", "profilePicture", "Profilképed")}
+        
+        ${selectElement("select", "where", "Hol hallottál rólunk", ["Interneten", "Ismerőstől", "Egyéb"])}
         <button>Ok</button>
     </form>
 `;
+ */
+
+const formElement = (ffs, id) => {
+    let inputs = ``;
+
+    for (const ff of ffs) {
+        inputs += inputElement(ff.type, ff.name, ff.label, ff.placeholder, ff.required);
+    };
+
+    return `
+    <form id="${id}">
+        <h3>Jelentkezz ünnepi hírlevelünkre!</h3>
+        ${inputs}
+        ${selectElement("select", "where", "Hol hallottál rólunk", ["Interneten", "Ismerőstől", "Egyéb"])}
+        <button>Ok</button>
+    `;
+};
 
 const formSubmit = (event) => {
     event.preventDefault();
@@ -80,6 +156,18 @@ const inputUpdate = (event) => {
         inputValue.innerHTML = `Üdv ${event.target.value}!`;
     };
 
+    if (event.target.getAttribute("name") === "profilePicture") {
+        console.log(event.target.files[0]);
+        const image = URL.createObjectURL(event.target.files[0]);
+            // FileReader.readAsDataURL() is jó
+
+
+        document.getElementById("inputValue").insertAdjacentHTML("beforeend", `
+        <img src="${image}">
+        `);
+    };
+
+
     console.log(event.target.closest(`#form`));
 }
 
@@ -88,7 +176,8 @@ function loadEvent() {
     console.log("Loaded");
     
     const rootElement = document.getElementById("root");
-    rootElement.insertAdjacentHTML("beforeend", formElement);
+    rootElement.insertAdjacentHTML("beforeend", formElement(formFields, "form"));
+    rootElement.insertAdjacentHTML("beforeend", formElement(anotherFormFields, "form2"));
     
     rootElement.querySelector("h3").insertAdjacentHTML("beforebegin", `
         <h2 id="inputValue">Üdv!</h2>
